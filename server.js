df78fb39c852f65a5aed144b920b2b14ae9082ea
ziruochou <ziruo@live.unc.edu>
2021-11-30 20:26:37 -0500

    2

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.get("/app/", (req, res, next) => {
 });
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
+
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
@@ -34,7 +35,10 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-
+app.get("/app/users", (req, res) => {	
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id=2").get();
+	res.status(200).json(stmt);
+});
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
