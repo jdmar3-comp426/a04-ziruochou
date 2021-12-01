94d89ba35cf7b7cce9eaf7526b03f12adfbd674a
ziruochou <ziruo@live.unc.edu>
2021-11-30 20:46:39 -0500

    commit

--- server.js
+++ server.js
@@ -35,10 +35,8 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/users", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id=2").get();
-	res.status(200).json(stmt);
-});
+const stmt = db.prepare('SELECT * FROM userinfo WHERE id = 2');
+const name = stmt.get();
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
