d9606bf1eaee4d6428c8f86ea361a970f6b53b94
ziruochou <ziruo@live.unc.edu>
2021-11-30 20:15:36 -0500

    get

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 
+app.get("/app/users", (req, res) => {	
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id=2").get();
+	res.status(200).json(stmt);
+});
+
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
