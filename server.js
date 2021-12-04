e0f36845afcd1ea2ddbf9b809e5a4470629ab1b9
ziruochou <ziruo@live.unc.edu>
2021-12-03 19:41:01 -0500

    ''

--- server.js
+++ server.js
@@ -53,13 +53,13 @@ res.status(200).json(name);
 app.patch("/app/update/user/:id",(req, res)=>{
 	const stmt = db.prepare('UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?');
     const out = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(200).json({"message": out.changes + " record updated: ID " + req.params.id +" (200)"});
+	res.status(200).json({"message": out.changes + " record updated: ID " + req.params.id + " (200)"});
 })
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 //DELETE FROM userinfo WHERE id = ?
 app.delete("/app/delete/user/:id", (req, res) => {
-	const stmt = db.prepare("DELETE * FROM userinfo WHERE id = ?");
+	const stmt = db.prepare('DELETE * FROM userinfo WHERE id = ?');
 	const out = stmt.run(req.params.id);
 	res.status(200).json({"message": out.changes + " record deleted: ID " + req.params.id + " (200)"});
 	})
